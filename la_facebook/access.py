34123fb2089768a1343197b8842f92a5203bd605
ptone <preston@ptone.com>
2011-02-20 20:51:45 -0800

    removed LinkedIn specific token handling

--- la_facebook/access.py
+++ la_facebook/access.py
@@ -242,15 +242,7 @@ class OAuthAccess(object):
             http = httplib2.Http()
             response, content = http.request(url, **request_kwargs)
         else:
-            if isinstance(token, basestring):
-                token = oauth.Token.from_string(token)
-            client = Client(self.consumer, token=token)
-            # @@@ LinkedIn requires Authorization header which is supported in
-            # sub-classed version of Client (originally from oauth2)
-            request_kwargs = dict(method=method, force_auth_header=True)
-            if method == "POST":
-                request_kwargs["body"] = urllib.urlencode(kwargs["params"])
-            response, content = client.request(url, **request_kwargs)
+            raise ValueError("an OAuth20Token is required for API call")
         if response["status"] == "401":
             raise NotAuthorized()
         if not content:
@@ -270,59 +262,6 @@ class OAuthAccess(object):
             return etree.ElementTree(etree.fromstring(content))
         else:
             raise Exception("unsupported API kind")
-
-
-class Client(oauth.Client):
-    """
-    Custom client to support forcing Authorization header (which is required
-    by LinkedIn). See http://github.com/brosner/python-oauth2/commit/82a05f96878f187f67c1af44befc1bec562e5c1f
-    """
-    
-    def request(self, uri, method="GET", body=None, headers=None,
-      redirections=httplib2.DEFAULT_MAX_REDIRECTS, connection_type=None,
-      force_auth_header=False):
-        
-        DEFAULT_CONTENT_TYPE = "application/x-www-form-urlencoded"
-        
-        if not isinstance(headers, dict):
-            headers = {}
-        
-        is_multipart = method == "POST" and headers.get("Content-Type", DEFAULT_CONTENT_TYPE) != DEFAULT_CONTENT_TYPE
-        
-        if body and method == "POST" and not is_multipart:
-            parameters = dict(urlparse.parse_qsl(body))
-        else:
-            parameters = None
-        
-        req = oauth.Request.from_consumer_and_token(self.consumer,
-            token=self.token, http_method=method, http_url=uri,
-            parameters=parameters)
-        
-        req.sign_request(self.method, self.consumer, self.token)
-        
-        if force_auth_header:
-            headers.update(req.to_header())
-        
-        if method == "POST":
-            headers["Content-Type"] = headers.get("Content-Type", DEFAULT_CONTENT_TYPE)
-            if is_multipart:
-                headers.update(req.to_header())
-            else:
-                if not force_auth_header:
-                    body = req.to_postdata()
-                else:
-                    body = urllib.urlencode(req.get_nonoauth_parameters(), True)
-        elif method == "GET":
-            if not force_auth_header:
-                uri = req.to_url()
-        else:
-            if not force_auth_header:
-                # don't call update twice.
-                headers.update(req.to_header())
-        
-        return httplib2.Http.request(self, uri, method=method, body=body,
-            headers=headers, redirections=redirections,
-            connection_type=connection_type)
 
 
 class OAuth20Token(object):
