f2a20a6dbd729a23b684081dc392443110abc342
ptone <preston@ptone.com>
2011-03-02 15:14:17 -0800

    remove old settings related docstring

--- la_facebook/exceptions.py
+++ la_facebook/exceptions.py
@@ -1,17 +1,6 @@
 class FacebookSettingsKeyError(KeyError):
-    """ Expect to see at least:
-    
-    FACEBOOK_ACCESS_SETTINGS = {
-        "keys": {
-            "FACEBOOK_APP_ID": <MY_FACEBOOK_APP_ID>,
-            "FACEBOOK_APP_SECRET": <MY_FACEBOOK_APP_SECRET>,
-        },
-        "endpoints": {
-            # Will need to redo the following
-            "callback": "fb_la_test.oauth.facebook_callback",
-            # Probably too much power here - just need to have authentication
-        }
-    }
+    """ 
+    see settings docs
     """
     pass
 
