4c5f1f18566a82d5888cf2d4c28fb435598d70e2
ptone <preston@ptone.com>
2011-03-03 21:24:17 -0800

    large integrated test of default callback

--- la_facebook/tests/test_callbacks.py
+++ la_facebook/tests/test_callbacks.py
@@ -22,7 +22,7 @@ from la_facebook.models import UserAssociation
 factory = RequestFactory()
 
 mock_fetch_user_data = Mock()
-mock_fetch_user_data.return_value = {'id':'facebookid'}
+mock_fetch_user_data.return_value = {'id':'facebookid','color':'red'}
 
 
 class BaseCallbackTests(TestCase):
@@ -41,7 +41,7 @@ class BaseCallbackTests(TestCase):
         assoc.token = 'facebooktokenstring'
         assoc.expires = datetime.datetime.now() + datetime.timedelta(1)
         assoc.save()
-        self.token = assoc.token
+        self.token = OAuth20Token(assoc.token, 5555)
         self.access = OAuthAccess()
 
     def test_call(self):
@@ -104,7 +104,7 @@ class DefaultCallbackTests(TestCase):
         assoc.identifier = 'facebookid'
         assoc.expires = datetime.datetime.now() + datetime.timedelta(1)
         assoc.save()
-        self.token = assoc.token
+        self.token = OAuth20Token(assoc.token, 5555)
         self.access = OAuthAccess()
 
     def test_lookup_user_exists(self):
@@ -117,14 +117,18 @@ class DefaultCallbackTests(TestCase):
         user = callback.lookup_user(self.request, self.access,{'id':'bad-id'})
         self.assertEquals(user,None)
 
+    @patch(
+       'la_facebook.callbacks.default.DefaultFacebookCallback.fetch_user_data',
+       mock_fetch_user_data)
     def test_update_profile_from_graph(self):
         callback = DefaultFacebookCallback()
         class DummyProfile(object):
             def __init__(self):
                 self.color = "blue"
         profile = DummyProfile()
-        # TODO need mock user data for this
-        pass
+        ret_p = callback.update_profile_from_graph(self.request, self.access,
+                self.token, profile)
+        self.assertEquals(ret_p.color,'red')
 
     @patch(
        'la_facebook.callbacks.default.DefaultFacebookCallback.fetch_user_data',
@@ -134,4 +138,40 @@ class DefaultCallbackTests(TestCase):
         ud = callback.fetch_user_data(self.request, self.access, 'faketoken')
         self.assertEquals(ud['id'], 'facebookid')
 
-
+    @patch(
+       'la_facebook.callbacks.default.DefaultFacebookCallback.fetch_user_data',
+       mock_fetch_user_data)
+    def test_handle_no_user(self):
+        """
+        This is a complex functional/integration test, hitting the following:
+            create_user
+                identifier_from_data
+                create_profile
+                    update_profile_from_graph
+                handle_unauthenticated_user
+                    login_user
+                    persist
+        """
+        class session_like_obj(dict):
+            # an alternative to http://code.djangoproject.com/ticket/10899
+            def __init__(self):
+                dict.__init__(self)
+                self['_auth_user_id'] = 'notme'
+                self.flush = lambda: True
+                self.set_expiry = Mock()
+        pseudo_session = session_like_obj()
+        self.request.session = pseudo_session
+        callback = DefaultFacebookCallback()
+        user_data = {'name':'new_user','id':'newfacebookid'}
+        resp = callback.handle_no_user(self.request, self.access, self.token,
+                user_data)
+        ident = callback.identifier_from_data(user_data)
+        expected_user = User.objects.get(username=ident)
+        # was the expected user created and returned
+        self.assertEquals(resp,expected_user)
+        # was the set_expiry method called on the session
+        # could check expected date also
+        self.assertTrue(pseudo_session.set_expiry.called)
+        # check that user data was persisted
+        assoc_obj = UserAssociation.objects.get(identifier=user_data['id'])
+        self.assertEquals(self.token.expires, assoc_obj.expires)
